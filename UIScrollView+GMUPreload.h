4382aa56ab05de718ce04e3abb8d0c9afeff6486
FaceBook <tao_shuminX@foxmail.com>
2018-12-19 09:23:47 +0800

    no message

---
+++ UIScrollView+GMUPreload.h
@@ -0,0 +1,36 @@
+//
+//  UIScrollView+GMUPreload.h
+//  gmu
+//
+//  Created by FaceBook on 2018/12/13.
+//  Copyright © 2018 FaceBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void(^PreloadBlock)(void);
+
+@interface UIScrollView (GMUPreload)
+
+/**
+提前多少值开始预加载
+ */
+@property(nonatomic,copy)PreloadBlock preloadBlock;
+
+/**
+ 预加载
+ */
+@property(nonatomic,strong)NSNumber *preloadMinCount;
+/**
+ */
+@property (nonatomic, strong) NSArray *datas;
+/**
+ @param currentIndex currentIndex  当前选择位置
+ */
+- (void)preloadDataWithCurrentIndex:(NSInteger)currentIndex;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIScrollView+GMUPreload.m
@@ -0,0 +1,62 @@
+//
+//  UIScrollView+GMUPreload.m
+//  gmu
+//
+//  Created by FaceBook on 2018/12/13.
+//  Copyright © 2018 FaceBook. All rights reserved.
+//
+
+#import "UIScrollView+GMUPreload.h"
+
+#import <objc/runtime.h>
+
+static const char * keyDataArray = "keyDataArray";
+
+static const char * keyPreloadBlock = "keyPreloadBlock";
+
+static const char * keyPreloadMinCount = "preloadMinCount";
+
+@implementation UIScrollView (GMUPreload)
+
+-(PreloadBlock)preloadBlock{
+    return objc_getAssociatedObject(self, &keyPreloadBlock);
+}
+
+-(void)setPreloadBlock:(PreloadBlock)preloadBlock{
+  objc_setAssociatedObject(self, &keyPreloadBlock, preloadBlock, OBJC_ASSOCIATION_COPY);
+}
+-(void)setPreloadMinCount:(NSNumber *)preloadMinCount{
+    
+    objc_setAssociatedObject(self, &keyPreloadMinCount, preloadMinCount, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+}
+
+-(NSNumber*)preloadMinCount{
+
+    return objc_getAssociatedObject(self, &keyPreloadMinCount);
+    
+}
+- (NSArray *)datas{
+    return objc_getAssociatedObject(self, &keyDataArray);
+}
+-(void)setDatas:(NSArray *)datas{
+   objc_setAssociatedObject(self, &keyDataArray, datas, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+
+- (void)preloadDataWithCurrentIndex:(NSInteger)currentIndex{
+    
+    NSInteger totalCount = self.datas.count;
+    
+    NSLog(@"%ld----------%d",currentIndex+1,[self  isPreloadDataWithTotalCount:totalCount currentIndex:currentIndex+1]);
+    
+    if ([self  isPreloadDataWithTotalCount:totalCount currentIndex:currentIndex+1]&&self.preloadBlock) {
+        self.preloadBlock();
+    }
+}
+- (BOOL)isPreloadDataWithTotalCount:(NSInteger)totalCount currentIndex:(NSInteger)currentIndex{
+    
+    return  ((currentIndex == totalCount - self.preloadMinCount.integerValue) && (currentIndex >= self.preloadMinCount.integerValue));
+    
+}
+@end
